b14b23d5c3d096cae3a65f4f49fa6fef0ac37ae0
FernandaMontesZ <fernanda.moz08@outlook.com>
2021-01-21 18:36:17 -0600

    CRUDdx v1.0

---
+++ CRUD/Controllers/DEpersonalController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class DEpersonalController : Controller
+    {
+        private EmpleadosEntities db = new EmpleadosEntities();
+        private consultasSQL emDB = new consultasSQL();
+
+        // GET: DEpersonal
+        public ActionResult Index()
+        {
+            return View(db.Personal.ToList());
+        }
+        public JsonResult ReadListaPersonal()
+        {
+            var result = emDB.ReadAll();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public ActionResult Create(Personal personal)
+        {
+            var result = emDB.Create(personal);
+            return Json(new { result = result }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetInfID(int? ID_personal)
+        {
+            List<Personal> personal = new List<Personal>();
+            if (ID_personal == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            personal = emDB.Details((int)ID_personal);
+            if (personal == null)
+            {
+                return HttpNotFound();
+            }
+            return Json(personal, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Personal personal)
+        {            
+            var result = emDB.Update(personal);
+            return Json(new { result = result }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Delete(int? ID)
+        {
+            List<Personal> personal = new List<Personal>();
+
+            if (ID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            personal = emDB.Details((int)ID);
+            if (personal == null)
+            {
+                return HttpNotFound();
+            }
+            var result = emDB.Delete((int)ID);
+            return Json(new { result = result }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUD/Scripts/dataGrid.js
@@ -0,0 +1,188 @@
+﻿$("#gridContainer").dxDataGrid({
+    dataSource: "/DEpersonal/ReadListaPersonal",
+    allowColumnReordering: true,
+    allowColumnResizing: true,
+    columnAutoWidth: true,
+    showBorders: true,
+    columnChooser: {
+        enabled: true
+    },
+    columnFixing: {
+        enabled: true
+    },
+    columns: [
+        //{
+        //    caption: "Nombre Personal",
+        //    width: 230,
+        //    fixed: true,
+        //    calculateCellValue: function (data) {
+        //        return [data.Nombre,
+        //        data.ApePaterno, data.ApeMaterno]
+        //            .join(" ");
+        //    }
+        //}
+        {
+            dataField: "Nombre",
+            dataType: "string",
+            alignment: "center",
+        },
+        {
+            dataField: "ApePaterno",
+            dataType: "string",
+            alignment: "center",
+        },
+        {
+            dataField: "ApeMaterno",
+            dataType: "string",
+            alignment: "center",
+        }
+        , {
+            dataField: "Edad",
+            dataType: "number",
+            alignment: "center",
+        }, {
+            dataField: "IsActive",
+            dataType: "string",
+            alignment: "center",
+        }]
+});
+
+//FUNCION GRID CON BOTONES
+            $(function () {
+                $(function () {
+                    function addRequestVerificationToken(data) {
+                        data.__RequestVerificationToken = $('input[name=__RequestVerificationToken]').val();
+                        return data;
+                    };
+                    var selectedRowIndex = -1;
+                    var dataSource = new DevExpress.data.DataSource({
+                        key: "ID_personal",
+                        load: function () {
+                            var d = $.Deferred();
+                            return $.ajax({
+                                url: '/DEpersonal/ReadListaPersonal'
+                            }).done(function (result) {
+                                d.resolve(result);
+                            }).fail(function () {
+                                throw "Data loading error";
+                            });
+                        },
+                        insert: function (values) {
+                            return $.ajax({
+                                type: "POST",
+                                url: "/DEpersonal/Create",
+                                data: addRequestVerificationToken({ personal: values }),
+                            })
+                        },
+                        update: function (key, values) {
+                            return $.ajax({
+                                type: "POST",
+                                url: "/DEpersonal/Edit",
+                                data: JSON.stringify({ ID_personal: key, personal: values }),
+                            })
+                        },
+                        remove: function (key) {
+                            return $.ajax({
+                                url: "/DEpersonal/Delete/"+encodeURIComponent(key),
+                                data: addRequestVerificationToken({ ID: key })
+                            })
+                        }
+                    });
+                    var IsActive = [{
+                        "ID": true,
+                        "Name": "Activo"
+                    }, {
+                        "ID": false,
+                        "Name": "No Activo"
+                    }];
+                    $("#action-add").dxSpeedDialAction({
+                        label: "Add row",
+                        icon: "add",
+                        index: 1,
+                        onClick: function () {
+                            grid.addRow();
+                            grid.deselectAll();
+                        }
+                    }).dxSpeedDialAction("instance");
+
+                    var deleteSDA = $("#action-remove").dxSpeedDialAction({
+                        icon: "trash",
+                        label: "Delete row",
+                        index: 2,
+                        visible: false,
+                        onClick: function () {
+                            grid.deleteRow(selectedRowIndex);
+                            grid.deselectAll();
+                        }
+                    }).dxSpeedDialAction("instance");
+
+                    var editSDA = $("#action-edit").dxSpeedDialAction({
+                        label: "Edit row",
+                        icon: "edit",
+                        index: 3,
+                        visible: false,
+                        onClick: function () {
+                            grid.editRow(selectedRowIndex);
+                            grid.deselectAll();
+                        }
+                    }).dxSpeedDialAction("instance");
+
+                    var grid = $("#grid").dxDataGrid({
+                        dataSource: dataSource,
+                        showBorders: true,
+                        selection: {
+                            mode: "single"
+                        },
+                        paging: {
+                            enabled: false
+                        },
+                        editing: {
+                            mode: "popup",
+                            texts: {
+                                confirmDeleteMessage: ""
+                            }
+                        },
+                        onSelectionChanged: function (e) {
+                            selectedRowIndex = e.component.getRowIndexByKey(e.selectedRowKeys[0]);
+
+                            deleteSDA.option("visible", selectedRowIndex !== -1);
+                            editSDA.option("visible", selectedRowIndex !== -1);
+                        },
+                        columns: [
+                            {
+                            dataField: "Nombre",
+                            dataType: "string",
+                            alignment: "center",
+                        },
+                        {
+                            caption: "Apellido Paterno",
+                            dataField: "ApePaterno",
+                            dataType: "string",
+                            alignment: "center",
+                        },
+                        {
+                            caption: "Apellido Materno",
+                            dataField: "ApeMaterno",
+                            dataType: "string",
+                            alignment: "center",
+                        }
+                            , {
+                            dataField: "Edad",
+                            dataType: "number",
+                            alignment: "center",
+                        },
+                        {
+                            dataField: "IsActive",
+                            caption: "Is Active",
+                            width: 125,
+                            lookup: {
+                                dataSource: IsActive,
+                                displayExpr: "Name",
+                                valueExpr: "ID"
+                            }
+                        }]
+                    }).dxDataGrid("instance");
+
+
+                });
+            });
